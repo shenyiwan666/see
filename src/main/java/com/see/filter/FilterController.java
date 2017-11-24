a144a82cb8bf4259072bdaa2c2b3ecad4141bfda
sam <2014141453044@stu.scu.edu.cn>
2017-11-24 11:43:03 +0800

    加入策划阶段

--- src/main/java/com/see/filter/FilterController.java
+++ src/main/java/com/see/filter/FilterController.java
@@ -30,16 +30,20 @@ public class FilterController extends HttpServlet implements Filter {
 		HttpServletResponse response=(HttpServletResponse)resp;
 		String URI = request.getRequestURI();
 		
-        if ("/login".equals(URI) || "/register".equals(URI)) {
-            chain.doFilter(request, response);
-            return;
-        } else {
-            Object obj = request.getSession().getAttribute("account");
-            if (obj == null) {
-                request.getRequestDispatcher("/WEB-INF/views/error.jsp").forward(request, response);
-            } else
-                chain.doFilter(request, response);
-        }
-	
+		
+		System.out.print(URI);
+		
+		
+	        if ("/login".equals(URI) || "/register".equals(URI) || URI.indexOf("resources") > 0 || URI.indexOf(".jsp") > 0) {
+	            chain.doFilter(request, response);
+	            return;
+	        } else {
+	            Object obj = request.getSession().getAttribute("account");
+	            if (obj == null) {
+	                request.getRequestDispatcher("/WEB-INF/views/error.jsp").forward(request, response);
+	            } else
+	                chain.doFilter(request, response);
+	        }
 	}
+	
 }
